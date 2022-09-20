ef1f794f0bdc94d84a3aae2a943091f4b2f60e7f
Suraj <surajpratap.org@gmail.com>
2022-09-20 16:19:03 +0530

    Completed Javascript code in this Project

---
+++ script.js
@@ -0,0 +1,22 @@
+'use strict';
+let show_modal = document.querySelectorAll('.show-modal');
+
+function remove_modal(){
+    document.querySelector('.modal').classList.remove('hidden')
+    document.querySelector('.overlay').classList.remove('hidden')
+}
+
+function add_modal(){
+    document.querySelector('.modal').classList.add('hidden')
+    document.querySelector('.overlay').classList.add('hidden')
+}
+
+for (let i = 0; i < show_modal.length; i++) {
+    show_modal[i].addEventListener('click', remove_modal)
+}
+
+document.querySelector('.close-modal').addEventListener('click', add_modal)
+
+
+document.querySelector('.overlay').addEventListener('click', add_modal)
+
